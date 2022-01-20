52b229dea3fb1185ac0e4b7e7914f250294c5715
Alvin <alvinhtml@gmail.com>
2022-01-20 23:55:19 +0800

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,122 +11,126 @@ const FileManagerPlugin = require('filemanager-webpack-plugin'); //webpack copy 
 
 module.exports = {
 
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin()
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+
+  mode: 'development',
+
+  entry: {
+    index: './src/index.js'
+  },
+
+  // devtool: 'source-map',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    library: 'react-miniui',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    modules: [path.resolve('node_modules')],
+    extensions: ['.js', '.jsx', '.scss', '.css'] //配置省略后缀名
+  },
+
+  module: {
+
+    rules: [{
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
         ]
-    },
-
-    mode: 'development',
-
-    entry: {
-        index: './src/index.js'
-    },
-
-    // devtool: 'source-map',
-
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        library: 'react-miniui',
-        libraryTarget: 'umd'
-    },
-
-    resolve: {
-        modules: [path.resolve('node_modules')],
-        extensions: ['.js', '.jsx', '.scss', '.css'] //配置省略后缀名
-    },
-
-    module: {
-
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader'
-                ]
-            },
-            {
-                test: /\.(scss|sass)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                            '@babel/preset-flow'
-                        ],
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-syntax-dynamic-import'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.(jpg|png|gif|jpeg|bmp|eot|svg|ttf|woff|woff2)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 20 * 1024,
-                        outputPath: './',
-                    }
-                }
-            }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
-    },
-
-    externals: {
-        react: 'react',
-        'react-dom': 'react-dom'
-    },
-
-
-    plugins: [ //数组，放着所有 webpack 插件
-        new MiniCssExtractPlugin({
-            filename: 'miniui.css'
-        }),
-        new FileManagerPlugin({
-          events: {
-            onEnd: {
-              copy: [
-                { source: '/dist/index.js', destination: '/Users/alvin/coder/alvin-project/react-miniui-docs/node_modules/react-miniui/dist' },
-                { source: '/dist/miniui.css', destination: '/Users/alvin/coder/alvin-project/react-miniui-docs/node_modules/react-miniui/dist' },
-              ]
-            }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-flow'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import'
+            ]
           }
-        })
-    ],
+        }
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|bmp|eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 20 * 1024,
+            outputPath: './',
+          }
+        }
+      }
+    ]
+  },
 
-    watch: true,
-    watchOptions: {
-        poll: 2000,
-        aggregateTimeout: 2000,
-        ignored: /node_modules|vendor|build|public|resources/
-    },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom'
+  },
 
-    devServer: {
-        port: 8080,
-        progress: true,
-        contentBase: './dist',
-        open: true,
-        historyApiFallback: true
-    }
+
+  plugins: [ //数组，放着所有 webpack 插件
+    new MiniCssExtractPlugin({
+      filename: 'miniui.css'
+    }),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [{
+              source: '/dist/index.js',
+              destination: '/Users/alvin/coder/alvin-project/react-miniui-docs/node_modules/react-miniui/dist'
+            },
+            {
+              source: '/dist/miniui.css',
+              destination: '/Users/alvin/coder/alvin-project/react-miniui-docs/node_modules/react-miniui/dist'
+            },
+          ]
+        }
+      }
+    })
+  ],
+
+  watch: true,
+  watchOptions: {
+    poll: 2000,
+    aggregateTimeout: 2000,
+    ignored: /node_modules|vendor|build|public|resources/
+  },
+
+  devServer: {
+    port: 8080,
+    progress: true,
+    contentBase: './dist',
+    open: true,
+    historyApiFallback: true
+  }
 }
